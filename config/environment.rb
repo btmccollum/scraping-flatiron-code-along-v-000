6566290571f376f5e8ebffa31c44e50f417bcb24
Brad McCollum <btmcco8@gmail.com>
2018-08-30 16:34:30 +0000

    automatically backed up by learn

---
+++ config/environment.rb
@@ -0,0 +1,6 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+# require_relative './course.rb'
+# require_relative './scraper.rb'
--- lib/course.rb
+++ lib/course.rb
@@ -1,4 +1,5 @@
 class Course
+<<<<<<< HEAD
   attr_accessor :title, :schedule, :description
   @@all = []
 
@@ -6,6 +7,26 @@ class Course
     @@all << self
   end
 
+=======
+
+  attr_accessor :title, :schedule, :description
+
+  @@all = []
+
+  def initialize
+    @@all << self
+  end
+
+  # def title
+  # end
+  #
+  # def schedule
+  # end
+  #
+  # def description
+  # end
+
+>>>>>>> 86c45e7187ad59080f69b72231f9675dcba21e34
   def self.all
     @@all
   end
