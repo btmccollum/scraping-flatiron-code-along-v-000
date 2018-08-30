e93e76791ca40385080b532ee99b351166831d5a
Brad McCollum <btmcco8@gmail.com>
2018-08-30 16:36:07 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,5 +1,4 @@
 class Course
-<<<<<<< HEAD
   attr_accessor :title, :schedule, :description
   @@all = []
 
@@ -7,26 +6,6 @@ class Course
     @@all << self
   end
 
-=======
-
-  attr_accessor :title, :schedule, :description
-
-  @@all = []
-
-  def initialize
-    @@all << self
-  end
-
-  # def title
-  # end
-  #
-  # def schedule
-  # end
-  #
-  # def description
-  # end
-
->>>>>>> 86c45e7187ad59080f69b72231f9675dcba21e34
   def self.all
     @@all
   end
